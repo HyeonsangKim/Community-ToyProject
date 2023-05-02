a426ea4cbbaaca0866f654a246e86ee79ada8a52
Kim HyeonSang <k98hh@naver.com>
2023-05-02 18:47:04 +0900

    기본 세팅 완료

--- README.md
+++
@@ -1,3 +0,0 @@
-# UsedMarket-ToyProject
-
-중고마켓 토이프로젝트
---
+++ used-market/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { Global } from "@emotion/react";
+import type { AppProps } from "next/app";
+import { globalStyles } from "../src/commons/styles/globalStyles";
+import Layout from "../src/components/commons/layout";
+import { RecoilRoot } from "recoil";
+import ApolloSetting from "../src/components/commons/apollo";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <ApolloSetting>
+        <>
+          <Global styles={globalStyles} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
+      </ApolloSetting>
+    </RecoilRoot>
+  );
+}
